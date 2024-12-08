3f8939a0d0f7f2bb94adfb953fc1b3e9acb5d123
vancoder1 <ivanzaporozhets25@gmail.com>
2024-12-07 17:24:34 -0700

    Add and position header, sidebar, main content window and footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,9 @@
-$(document).ready(function() {
-        $('#feedbackForm').on('submit', function(event) {
-            event.preventDefault();
-
-            const firstName = $('#firstName').val();
-            const lastName = $('#lastName').val();
-            const email = $('#email').val();
-            const subject = $('#subject').val();
-            const message = $('#message').val();
-
-            const fullName = `${firstName} ${lastName}`;
-            $('#submitterName').text(fullName);
-
-            if (firstName.length < 2 || lastName.length < 2 ||
-                email.length < 5 || message < 2) {
-                $('#feedbackForm').show();
-                $('#wrongInputMessage').show();
-                $('#thankYouMessage').hide();
-            }
-            else {
-                $('#feedbackForm').hide();
-                $('#wrongInputMessage').hide();
-                $('#thankYouMessage').show();
-            }
-            
-
-            console.log('Form submitted', {
-                name: fullName,
-                email: email,
-                subject: subject,
-                message: message
-            });
-        });
+$(document).ready(function () {
+    $('.mobile-menu-toggle').on('click', function () {
+        $('.sidebar, .mobile-sidebar-overlay').toggleClass('show');
     });
+
+    $('.mobile-sidebar-overlay').on('click', function () {
+        $('.sidebar, .mobile-sidebar-overlay').removeClass('show');
+    });
+});
